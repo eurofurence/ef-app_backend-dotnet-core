d710e491549ed00dd18a4ad71b931260853b241e
Luchs <luchs@pinselohr.ch>
2017-07-05 09:00:36 +0200

    Updating MapEntryRecord Description

--- src/Eurofurence.App.Domain.Model/Maps/MapEntryRecord.cs
+++ src/Eurofurence.App.Domain.Model/Maps/MapEntryRecord.cs
@@ -13,16 +13,16 @@ namespace Eurofurence.App.Domain.Model.Maps
         public Guid Id { get; set; }
 
         /// <summary>
-        ///     "X" coordinate of the *center* of a *circular area*, expressed as `[Relative Fraction of Map Image Width * 100]`.
-        ///       *  A value of `RelativeX=50` indicates it's on the horizontal middle of a map, and results in an absolute X of 1000 on a map that is 2000 pixels wide,
+        ///     "X" coordinate of the *center* of a *circular area*, expressed as `[Relative Fraction of Map Image Width]`.
+        ///       *  A value of `RelativeX=0.5` indicates it's on the horizontal middle of a map, and results in an absolute X of 1000 on a map that is 2000 pixels wide,
         ///          or 500 on a map that is 1000 pixels wide. 0 would indicate the far left side, where as 100 indicates the far right side of the image.
         /// </summary>
         [DataMember]
         public double RelativeX { get; set; }
 
         /// <summary>
-        ///     "Y" coordinate of the *center* of a *circular area*, expressed as `[Relative Fraction of Map Image Height * 100]`.
-        ///      *  A value of `RelativeY=50` indicates it's on the vertical middle of a map, and results in an absolute Y of 1000 on a map that is 2000 pixels in height,
+        ///     "Y" coordinate of the *center* of a *circular area*, expressed as `[Relative Fraction of Map Image Height]`.
+        ///      *  A value of `RelativeY=0.5` indicates it's on the vertical middle of a map, and results in an absolute Y of 1000 on a map that is 2000 pixels in height,
         ///         or 500 on a map that is 1000 in height. 0 would indicate the top side, where as 100 indicates the bottom side of the image.
         /// </summary>
         [DataMember]
