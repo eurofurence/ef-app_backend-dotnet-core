cc6f3ce7a784869cdeee3ea81849a016060b742f
Fenrikur <ef-backend@fenrikur.de>
2019-06-16 16:33:01 +0200

    Added settings for app ids on Google Play and Apple iTunes

--- src/Eurofurence.App.Server.Services/Abstractions/ConventionSettings.cs
+++ src/Eurofurence.App.Server.Services/Abstractions/ConventionSettings.cs
@@ -8,13 +8,17 @@ namespace Eurofurence.App.Server.Services.Abstractions
         public string ConventionIdentifier { get; set; }
         public bool IsRegSysAuthenticationEnabled { get; set; }
         public string ApiBaseUrl { get; set; }
+        public string AppIdITunes { get; set; } 
+        public string AppIdPlay { get; set; }
 
         public static ConventionSettings FromConfiguration(IConfiguration configuration) 
             => new ConventionSettings()
             {
                 ConventionIdentifier = configuration["global:conventionIdentifier"],
                 IsRegSysAuthenticationEnabled = Convert.ToInt32(configuration["global:regSysAuthenticationEnabled"]) == 1,
-                ApiBaseUrl = configuration["global:apiBaseUrl"]
+                ApiBaseUrl = configuration["global:apiBaseUrl"],
+                AppIdITunes = configuration["global:appIdITunes"],
+                AppIdPlay = configuration["global:appIdPlay"]
             };
     }
 }
