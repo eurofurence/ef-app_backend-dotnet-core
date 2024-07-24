f601defa5ced133cd858314c46f3999107c05b0e
Meta <76539710+Metawolve@users.noreply.github.com>
2024-07-24 17:28:37 +0200

    fix: Included events in conference day / conference room / conference track query results

--- src/Eurofurence.App.Server.Services/Events/EventConferenceRoomService.cs
+++ src/Eurofurence.App.Server.Services/Events/EventConferenceRoomService.cs
@@ -2,18 +2,40 @@ using Eurofurence.App.Domain.Model.Events;
 using Eurofurence.App.Infrastructure.EntityFramework;
 using Eurofurence.App.Server.Services.Abstractions;
 using Eurofurence.App.Server.Services.Abstractions.Events;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eurofurence.App.Server.Services.Events
 {
     public class EventConferenceRoomService : EntityServiceBase<EventConferenceRoomRecord>,
         IEventConferenceRoomService
     {
+        private readonly AppDbContext _appDbContext;
+
         public EventConferenceRoomService(
             AppDbContext appDbContext,
             IStorageServiceFactory storageServiceFactory
         )
             : base(appDbContext, storageServiceFactory)
         {
+            _appDbContext = appDbContext;
+        }
+
+        public override async Task<EventConferenceRoomRecord> FindOneAsync(Guid id)
+        {
+            return await _appDbContext.EventConferenceRooms
+                .Include(eventConferenceRoom => eventConferenceRoom.Events)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(entity => entity.Id == id);
+        }
+
+        public override IQueryable<EventConferenceRoomRecord> FindAll()
+        {
+            return _appDbContext.EventConferenceRooms
+                .Include(eventConferenceRoom => eventConferenceRoom.Events)
+                .AsNoTracking();
         }
     }
 }
--- src/Eurofurence.App.Server.Services/Events/EventConferenceTrackService.cs
+++ src/Eurofurence.App.Server.Services/Events/EventConferenceTrackService.cs
@@ -2,18 +2,40 @@
 using Eurofurence.App.Infrastructure.EntityFramework;
 using Eurofurence.App.Server.Services.Abstractions;
 using Eurofurence.App.Server.Services.Abstractions.Events;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eurofurence.App.Server.Services.Events
 {
     public class EventConferenceTrackService : EntityServiceBase<EventConferenceTrackRecord>,
         IEventConferenceTrackService
     {
+        private readonly AppDbContext _appDbContext;
+
         public EventConferenceTrackService(
             AppDbContext appDbContext,
             IStorageServiceFactory storageServiceFactory
         )
             : base(appDbContext, storageServiceFactory)
         {
+            _appDbContext = appDbContext;
+        }
+
+        public override async Task<EventConferenceTrackRecord> FindOneAsync(Guid id)
+        {
+            return await _appDbContext.EventConferenceTracks
+                .Include(eventConferenceTrack => eventConferenceTrack.Events)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(entity => entity.Id == id);
+        }
+
+        public override IQueryable<EventConferenceTrackRecord> FindAll()
+        {
+            return _appDbContext.EventConferenceTracks
+                .Include(eventConferenceTrack => eventConferenceTrack.Events)
+                .AsNoTracking();
         }
     }
 }
