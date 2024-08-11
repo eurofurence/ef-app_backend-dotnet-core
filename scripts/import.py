daaeefe497f14a51ecb1369a54e84b19be5b5d3e
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-08-11 22:33:12 +0200

    chore(scripts): refactor import script into functions

--- scripts/import.py
+++ scripts/import.py
@@ -12,9 +12,15 @@ from jsonschema.validators import RefResolver
 from openapi_schema_validator import OAS30Validator
 
 logger = logging.getLogger(__name__)
+debug = False
+
+
+class ProcessImagesError(Exception):
+	"""General error encountered when processing an entities image IDs."""
 
 
 def parse_arguments():
+	global debug
 	"""Parse command line arguments.
 
 	Returns:
@@ -91,7 +97,164 @@ def parse_arguments():
 	if args.image_source is not None:
 		args.image_source = args.image_source.removesuffix("/")
 
+	debug = args.debug
+
 	return args
+
+
+def loadSchemas(api: urllib3.connectionpool.ConnectionPool):
+	try:
+		openapi_schema_response = api.request("GET", "/swagger/api/swagger.json")
+	except urllib3.exceptions.HTTPError:
+		logger.error(f"Failed to retrieve OpenAPI schema from {api.host}.", exc_info=debug)
+		sys.exit(22)
+
+	openapi_schema = json.loads(openapi_schema_response.data)
+	schemas = openapi_schema["components"]["schemas"]
+	types = {}
+
+	for api_path, verbs in openapi_schema["paths"].items():
+		type_name = api_path.removeprefix("/Api/")
+		if "/" in type_name:
+			continue
+
+		if "post" in verbs and "application/json" in verbs["post"]["requestBody"]["content"]:
+			types[type_name] = {
+				"schema": verbs["post"]["requestBody"]["content"]["application/json"]["schema"]["$ref"].removeprefix(
+					"#/components/schemas/"
+				),
+				"path": api_path,
+				"post": True,
+				"delete": (
+					"delete" in verbs
+					and "parameters" in verbs["delete"]
+					and verbs["delete"]["parameters"]["name"] == "id"
+					and verbs["delete"]["parameters"]["in"] == "path"
+				),
+			}
+
+	return schemas, types
+
+
+def getSourceData(http: urllib3.connectionpool.ConnectionPool, source: str):
+	if source.startswith("https://") or source.startswith("http://"):
+		try:
+			data_response = http.request("GET", source)
+		except urllib3.exceptions.HTTPError:
+			logger.error(f"Failed to retrieve source file from {source}.", exc_info=debug)
+			sys.exit(23)
+		return json.loads(data_response.data)
+	else:
+		with open(source, encoding="utf-8") as f:
+			return json.load(f)
+
+
+def processImages(
+	http: urllib3.connectionpool.ConnectionPool,
+	api: urllib3.connectionpool.ConnectionPool,
+	image_ids: list[str],
+	image_source: str,
+):
+	image_ids_new = []
+	for image_id in image_ids:
+		image_meta_response = http.request("GET", f"{image_source}/Api/Images/{image_id}")
+		if image_meta_response.status != 200:
+			logger.warning(
+				f"Failed to get image metadata for {image_id} from {image_source}: {image_meta_response.status}"
+			)
+			break
+
+		image_meta_data = json.loads(image_meta_response.data)
+		if "Url" in image_meta_data:
+			image_content_response = http.request("GET", image_meta_data["Url"])
+		else:
+			image_content_response = http.request("GET", f"{image_source}/Api/Images/{image_id}/Content")
+
+		if image_content_response.status != 200:
+			logger.warning(
+				f"Failed to get image data for {image_id} from {image_source}: {image_content_response.status}"
+			)
+			break
+
+		image_post_response = api.request(
+			"POST",
+			"/Api/Images",
+			fields={"file": (image_meta_data["InternalReference"], image_content_response.data)},
+		)
+		if image_post_response.status == 200:
+			image_id_new = json.loads(image_post_response.data)["Id"]
+			image_ids_new.append(image_id_new)
+			logger.info(
+				f"Successfully uploaded image {image_id} ({image_meta_data["InternalReference"]}) to {image_source} with new ID {image_id_new}."
+			)
+		else:
+			logger.warning(
+				f"Failed to upload {image_id} ({image_meta_data["InternalReference"]}) to {api.host}: {image_post_response.status}"
+			)
+			break
+
+	if len(image_ids_new) != len(image_ids):
+		logger.warning(
+			f"Number of uploaded images ({len(image_ids_new)}) does not match expected number of images ({len(image_ids)}). Rolling back…"
+		)
+		for image_id in image_ids_new:
+			image_delete_response = api.request("DELETE", f"{image_source}/Api/Images/{image_id}")
+			if image_delete_response.status == 204:
+				logger.info(
+					f"Successfully deleted image {image_id_new} ({image_meta_data["InternalReference"]}) from {image_source}."
+				)
+			else:
+				logger.warning(
+					f"Failed to delete {image_id_new} ({image_meta_data["InternalReference"]}) from {api.host}: {image_delete_response.status}"
+				)
+		raise ProcessImagesError()
+	else:
+		return image_ids_new
+
+
+def processData(
+	http: urllib3.connectionpool.ConnectionPool,
+	api: urllib3.connectionpool.ConnectionPool,
+	data: dict,
+	validator: OAS30Validator,
+	types: dict,
+	type_name: str,
+	image_source: str,
+):
+	for item in data:
+		try:
+			validator.validate(item)
+		except ValidationError:
+			logger.error(f"Unable to import {type_name} with ID {item["Id"]} due to failed validation.", exc_info=debug)
+			continue
+
+		if image_source is not None and item["ImageIds"] is not None and len(item["ImageIds"]) > 0:
+			if api.request("GET", f"{types[type_name]["path"]}/{item["Id"]}").status != 404:
+				logger.warning(f"{type_name} with ID {item["Id"]} seems to already exist, skipping image upload…")
+				continue
+			else:
+				logger.info(f"Found {len(item["ImageIds"])} image IDs on {type_name} with ID {item["Id"]}.")
+				try:
+					item["ImageIds"] = processImages(http, api, item["ImageIds"], image_source=image_source)
+				except ProcessImagesError:
+					logger.warning(f"Failed to process images for {type_name} with ID {item["Id"]}, skipping…")
+					continue
+
+		try:
+			api_response = api.request("POST", types[type_name]["path"], json=item)
+
+			if api_response.status != 200 and api_response.status != 204:
+				logger.error(
+					f"Failed to import {type_name} with ID {item["Id"]}: [{api_response.status}] {api_response.data.decode("UTF-8")}"
+				)
+				logger.debug(f"Problematic item: {json.dumps(item)}")
+			else:
+				logger.info(
+					f"Successfully imported {type_name} with ID {item["Id"]}: {api_response.data.decode('UTF-8')}."
+				)
+		except urllib3.exceptions.HTTPError:
+			logger.error(f"Failed to POST {type_name} with ID {item["Id"]}.", exc_info=debug)
+			continue
 
 
 def main():
@@ -104,35 +267,7 @@ def main():
 		urllib3.connection_from_url(args.api, headers={"Authorization": f"Bearer {args.token}"}) as api,
 		urllib3.PoolManager() as http,
 	):
-		try:
-			openapi_schema_response = api.request("GET", "/swagger/api/swagger.json")
-		except urllib3.exceptions.HTTPError:
-			logger.error(f"Failed to retrieve OpenAPI schema from {args.api}.", exc_info=args.debug)
-			sys.exit(22)
-
-		openapi_schema = json.loads(openapi_schema_response.data)
-		schemas = openapi_schema["components"]["schemas"]
-		types = {}
-
-		for api_path, verbs in openapi_schema["paths"].items():
-			type_name = api_path.removeprefix("/Api/")
-			if "/" in type_name:
-				continue
-
-			if "post" in verbs and "application/json" in verbs["post"]["requestBody"]["content"]:
-				types[type_name] = {
-					"schema": verbs["post"]["requestBody"]["content"]["application/json"]["schema"][
-						"$ref"
-					].removeprefix("#/components/schemas/"),
-					"path": api_path,
-					"post": True,
-					"delete": (
-						"delete" in verbs
-						and "parameters" in verbs["delete"]
-						and verbs["delete"]["parameters"]["name"] == "id"
-						and verbs["delete"]["parameters"]["in"] == "path"
-					),
-				}
+		schemas, types = loadSchemas(api)
 
 		if args.list:
 			type_info = reduce(
@@ -153,106 +288,9 @@ def main():
 		resolver = RefResolver.from_schema({"components": {"schemas": schemas}})
 		validator = OAS30Validator(schemas[types[args.type]["schema"]], resolver=resolver)
 
-		data = {}
-		if args.source.startswith("https://") or args.source.startswith("http://"):
-			try:
-				data_response = http.request("GET", args.source)
-			except urllib3.exceptions.HTTPError:
-				logger.error(f"Failed to retrieve source file from {args.source}.", exc_info=args.debug)
-				sys.exit(23)
-			data = json.loads(data_response.data)
-		else:
-			with open(args.source, encoding="utf-8") as f:
-				data = json.load(f)
+		data = getSourceData(http, args.source)
 
-		for item in data:
-			try:
-				validator.validate(item)
-			except ValidationError:
-				logger.error(
-					f"Unable to import {args.type} with ID {item["Id"]} due to failed validation.", exc_info=args.debug
-				)
-				continue
-
-			if args.image_source is not None and "ImageIds" in item and len(item["ImageIds"]) > 0:
-				if api.request("GET", f"{types[args.type]["path"]}/{item["Id"]}").status != 404:
-					logger.warning(f"{args.type} with ID {item["Id"]} seems to already exist, skipping image upload…")
-					continue
-
-				logger.info(f"Found {len(item["ImageIds"])} image IDs on {args.type} with ID {item["Id"]}.")
-				image_ids = []
-				for image_id in item["ImageIds"]:
-					image_meta_response = http.request("GET", f"{args.image_source}/Api/Images/{image_id}")
-					if image_meta_response.status != 200:
-						logger.warning(
-							f"Failed to get image metadata for {image_id} from {args.image_source}: {image_meta_response.status}"
-						)
-						continue
-
-					image_meta_data = json.loads(image_meta_response.data)
-					if "Url" in image_meta_data:
-						image_content_response = http.request("GET", image_meta_data["Url"])
-					else:
-						image_content_response = http.request(
-							"GET", f"{args.image_source}/Api/Images/{image_id}/Content"
-						)
-
-					if image_content_response.status != 200:
-						logger.warning(
-							f"Failed to get image data for {image_id} from {args.image_source}: {image_content_response.status}"
-						)
-						continue
-
-					image_post_response = api.request(
-						"POST",
-						"/Api/Images",
-						fields={"file": (image_meta_data["InternalReference"], image_content_response.data)},
-					)
-					if image_post_response.status == 200:
-						image_id_new = json.loads(image_post_response.data)["Id"]
-						image_ids.append(image_id_new)
-						logger.info(
-							f"Successfully uploaded image {image_id} ({image_meta_data["InternalReference"]}) to {args.image_source} with new ID {image_id_new}."
-						)
-					else:
-						logger.warning(
-							f"Failed to upload {image_id} ({image_meta_data["InternalReference"]}) to {args.api}: {image_post_response.status}"
-						)
-						continue
-
-				if len(item["ImageIds"]) != len(image_ids):
-					logger.warning(
-						f"Number of uploaded images ({len(image_ids)}) does not match expected number of images ({len(item["ImageIds"])}) for {args.type} with ID {item["Id"]}. Rolling back…"
-					)
-					for image_id in image_ids:
-						image_delete_response = api.request("DELETE", f"{args.image_source}/Api/Images/{image_id}")
-						if image_delete_response.status == 204:
-							logger.info(
-								f"Successfully deleted image {image_id_new} ({image_meta_data["InternalReference"]}) from {args.image_source}."
-							)
-						else:
-							logger.warning(
-								f"Failed to delete {image_id_new} ({image_meta_data["InternalReference"]}) from {args.api}: {image_delete_response.status}"
-							)
-					continue
-				else:
-					item["ImageIds"] = image_ids
-
-			try:
-				api_response = api.request("POST", types[args.type]["path"], json=item)
-
-				if api_response.status != 200 and api_response.status != 204:
-					logger.error(
-						f"Failed to import {args.type} with ID {item["Id"]}: [{api_response.status}] {api_response.data.decode("UTF-8")}"
-					)
-					logger.debug(f"Problematic item: {json.dumps(item)}")
-				else:
-					logger.info(
-						f"Successfully imported {args.type} with ID {item["Id"]}: {api_response.data.decode('UTF-8')}."
-					)
-			except urllib3.exceptions.HTTPError:
-				logger.error(f"Failed to POST {args.type} with ID {item["Id"]}.", exc_info=args.debug)
-				continue
+		processData(http, api, data, validator, types, type_name=args.type, image_source=args.image_source)
 
 
 if __name__ == "__main__":
