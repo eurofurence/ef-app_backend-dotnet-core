a91eb9ca52e6cd68e57f40bbd38dfdc5aa9169b6
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-08-16 00:43:45 +0200

    feat(users): endpoint to introspect token

---
+++ src/Eurofurence.App.Server.Web/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Eurofurence.App.Domain.Model.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eurofurence.App.Server.Web.Controllers
+{
+    [Route("Api/[controller]")]
+    public class UsersController : BaseController
+    {
+        [Authorize]
+        [HttpGet(":self")]
+        [ProducesResponseType(typeof(UserRecord), 200)]
+        public UserRecord GetUsersSelf()
+        {
+            var result = new UserRecord();
+            if (User.Identity is ClaimsIdentity identity)
+            {
+                result.Roles = identity.FindAll(identity.RoleClaimType).Select(c => c.Value).ToArray();
+                result.Registrations = identity.FindAll(UserRegistrationClaims.Id).Select(c =>
+                {
+                    var statusString = identity.FindFirst(UserRegistrationClaims.Status(c.Value))?.Value.Replace(" ", "");
+                    Enum.TryParse(statusString, true, out UserRegistrationStatus registrationStatus);
+                    var registration = new UserRegistration
+                    {
+                        Id = c.Value,
+                        Status = registrationStatus
+                    };
+                    return registration;
+                }).ToArray();
+            }
+            return result;
+        }
+    }
+}
