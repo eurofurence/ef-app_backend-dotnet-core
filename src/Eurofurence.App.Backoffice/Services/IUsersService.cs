22fcd2b1b1de0f5a4ae1bf07c037faf261891bfb
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-08-17 17:28:02 +0200

    feat(backoffice): authorize via backend roles

---
+++ src/Eurofurence.App.Backoffice/Services/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿using Eurofurence.App.Domain.Model.Users;
+
+namespace Eurofurence.App.Backoffice.Services
+{
+    public interface IUsersService
+    {
+        public Task<UserRecord> GetUserSelf();
+    }
+}
