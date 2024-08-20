9afe5f7bd9e8dc9195e2992ff0d819458458a61f
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-08-20 23:50:00 +0200

    chore: fix indentation in TableRegistrationRequest

--- src/Eurofurence.App.Domain.Model/ArtistsAlley/TableRegistrationRequest.cs
+++ src/Eurofurence.App.Domain.Model/ArtistsAlley/TableRegistrationRequest.cs
@@ -1,15 +1,15 @@
 ﻿namespace Eurofurence.App.Domain.Model.ArtistsAlley
 {
-        public class TableRegistrationRequest
-        {
-                public string DisplayName { get; set; }
+    public class TableRegistrationRequest
+    {
+        public string DisplayName { get; set; }
 
-                public string WebsiteUrl { get; set; }
+        public string WebsiteUrl { get; set; }
 
-                public string ShortDescription { get; set; }
+        public string ShortDescription { get; set; }
 
-                public string Location { get; set; }
+        public string Location { get; set; }
 
-                public string TelegramHandle { get; set; }
-        }
+        public string TelegramHandle { get; set; }
+    }
 }
