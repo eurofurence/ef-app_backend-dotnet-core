3219776ece31c409a49ac0b8305b093b3efd9cd8
Meta <76539710+Metawolve@users.noreply.github.com>
2024-08-22 07:54:19 +0200

    fix: Redirecting to login when no new token could be aquired

--- src/Eurofurence.App.Backoffice/Authentication/TokenAuthorizationMessageHandler.cs
+++ src/Eurofurence.App.Backoffice/Authentication/TokenAuthorizationMessageHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Security.Authentication;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace Eurofurence.App.Backoffice.Authentication
@@ -8,6 +9,19 @@ namespace Eurofurence.App.Backoffice.Authentication
         public TokenAuthorizationMessageHandler(IAccessTokenProvider provider, NavigationManager navigation, IConfiguration configuration) : base(provider, navigation)
         {
             ConfigureHandler(new[] { configuration.GetValue<string>("ApiUrl") ?? string.Empty });
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await base.SendAsync(request, cancellationToken);
+            }
+            catch (AccessTokenNotAvailableException ex)
+            {
+                ex.Redirect();
+                throw new AuthenticationException("Authentication has expired. Redirecting to login...");
+            }
         }
     }
 }
