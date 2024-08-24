1c0f564334bbc0d11dfea252bec5d0e211fb3934
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-08-24 17:41:42 +0200

    chore: standardize indentation in non-conforming files (#177)
    * chore: standardize indentation to spaces
    
    * chore: cleanup unused settings

--- src/Eurofurence.App.Domain.Model/Users/UserRegistration.cs
+++ src/Eurofurence.App.Domain.Model/Users/UserRegistration.cs
@@ -2,11 +2,11 @@ using System.Runtime.Serialization;
 
 namespace Eurofurence.App.Domain.Model.Users
 {
-	public class UserRegistration
-	{
-		[DataMember]
-		public string Id { get; set; }
-		[DataMember]
-		public UserRegistrationStatus Status { get; set; }
-	}
+    public class UserRegistration
+    {
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public UserRegistrationStatus Status { get; set; }
+    }
 }
