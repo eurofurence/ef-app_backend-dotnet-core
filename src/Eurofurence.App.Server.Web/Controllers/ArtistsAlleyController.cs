a8fe521255b111fa886002e3981d5d5cf9c06bdd
maakinoh <info@maakinoh.de>
2024-09-04 23:02:33 +0200

    feat: delete already approved/rejected artist alley regs (#215)
    * feat(artistalley): #197 allow delete all artist alley regs
    
    All Artist alley applications can now be deleted by an admin - even if already approved or rejected
    
    * fix(artistalley): #197 allow delete all artist alley regs
    
    Fixed role permissions at the artist alley endpoints

--- src/Eurofurence.App.Server.Web/Controllers/ArtistsAlleyController.cs
+++ src/Eurofurence.App.Server.Web/Controllers/ArtistsAlleyController.cs
@@ -59,7 +59,7 @@ namespace Eurofurence.App.Server.Web.Controllers
         /// <returns>All table registrations.</returns>
         [ProducesResponseType(typeof(string), 404)]
         [ProducesResponseType(typeof(IEnumerable<TableRegistrationRecord>), 200)]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin, ArtistAlleyModerator, ArtistAlleyAdmin")]
         [HttpGet]
         public IEnumerable<TableRegistrationRecord> GetTableRegistrationsAsync()
         {
@@ -72,7 +72,7 @@ namespace Eurofurence.App.Server.Web.Controllers
         /// <param name="id">id of the requested entity</param>
         [ProducesResponseType(typeof(string), 404)]
         [ProducesResponseType(typeof(TableRegistrationRecord), 200)]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin, ArtistAlleyModerator, ArtistAlleyAdmin")]
         [HttpGet("{id}")]
         public async Task<TableRegistrationRecord> GetTableRegistrationAsync(
             [EnsureNotNull][FromRoute] Guid id
