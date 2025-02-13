bd007a06173b1a0f7fe730900c003156bb812ec7
Maakinoh <29598855+maakinoh@users.noreply.github.com>
2025-02-13 19:41:13 +0100

    refactor(#284): removed old fields from TelegramUserRecord

--- src/Eurofurence.App.Domain.Model/Telegram/TelegramUserRecord.cs
+++ src/Eurofurence.App.Domain.Model/Telegram/TelegramUserRecord.cs
@@ -7,9 +7,5 @@ namespace Eurofurence.App.Domain.Model.Telegram
     {
         public string Username { get; set; }
         public string Acl { get; set; }
-
-        public string RegsysID { get; set; }
-
-        public List<EventRecord> FavoriteEvents { get; set; } = new();
     }
 }
