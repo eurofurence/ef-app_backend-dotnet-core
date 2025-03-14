25ccea962b4fddd6e3217db71fc075a5e920c25c
Maakinoh <29598855+maakinoh@users.noreply.github.com>
2025-03-14 23:21:45 +0100

    feat(#286): add new API endpoint /Api/ArtistsAlley/TableRegistration/:my-latest
    This endpoint may be used by users to delete their own artist alley table registrations.

--- src/Eurofurence.App.Server.Services/Abstractions/ArtistsAlley/ITableRegistrationService.cs
+++ src/Eurofurence.App.Server.Services/Abstractions/ArtistsAlley/ITableRegistrationService.cs
@@ -16,6 +16,14 @@ namespace Eurofurence.App.Server.Services.Abstractions.ArtistsAlley
         IQueryable<TableRegistrationRecord> GetRegistrations(TableRegistrationRecord.RegistrationStateEnum? state);
         Task<TableRegistrationRecord> GetLatestRegistrationByUidAsync(string uid);
 
+        /// <summary>
+        /// Deletes the latest registration of a given user by their ID (<paramref name="uid"/>)
+        /// </summary>
+        /// <param name="uid">The ID of the user whose registration should be deleted</param>
+        /// <exception cref="ArgumentException">Can be thrown when no registration can be found under the users <paramref name="uid"/></exception>
+        /// <returns></returns>
+        Task DeleteLatestRegistrationByUidAsync(string uid);
+
         Task ApproveByIdAsync(Guid id, string operatorUid);
         Task RejectByIdAsync(Guid id, string operatorUid);
     }
