4d9756796bb22d00cc1e20483a807c1c30ffcaa7
Meta <76539710+Metawolve@users.noreply.github.com>
2025-07-30 19:00:10 +0200

    fix(backoffice): Fixed request/response type mapping

--- src/Eurofurence.App.Backoffice/Services/IKnowledgeService.cs
+++ src/Eurofurence.App.Backoffice/Services/IKnowledgeService.cs
@@ -8,9 +8,9 @@ namespace Eurofurence.App.Backoffice.Services
         public Task<bool> PutKnowledgeEntryAsync(Guid id, KnowledgeEntryRequest record);
         public Task<bool> PostKnowledgeEntryAsync(KnowledgeEntryRequest record);
         public Task<bool> DeleteKnowledgeEntryAsync(Guid id);
-        public Task<KnowledgeGroupRecord[]> GetKnowledgeGroupsAsync();
-        public Task<bool> PutKnowledgeGroupAsync(KnowledgeGroupRecord record);
-        public Task<bool> PostKnowledgeGroupAsync(KnowledgeGroupRecord record);
+        public Task<KnowledgeGroupResponse[]> GetKnowledgeGroupsAsync();
+        public Task<bool> PutKnowledgeGroupAsync(Guid id, KnowledgeGroupRequest record);
+        public Task<bool> PostKnowledgeGroupAsync(KnowledgeGroupRequest record);
         public Task<bool> DeleteKnowledgeGroupAsync(Guid id);
     }
 }
