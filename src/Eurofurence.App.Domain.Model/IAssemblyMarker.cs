e733405c07917b01556f13f2dff05da0323bbfb1
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2025-07-30 13:05:10 +0200

    docs(model): clarify use of IAssemblyMarker

--- src/Eurofurence.App.Domain.Model/IAssemblyMarker.cs
+++ src/Eurofurence.App.Domain.Model/IAssemblyMarker.cs
@@ -1,5 +1,9 @@
 namespace Eurofurence.App.Domain.Model
 {
+    /// <summary>
+    /// Used by Eurofurence.App.Server.Web.Startup.ServiceCollectionExtensions to find the
+    /// type adapter configurations for Mapster in this assembly.
+    /// </summary>
     public interface IAssemblyMarker
     {
     }
