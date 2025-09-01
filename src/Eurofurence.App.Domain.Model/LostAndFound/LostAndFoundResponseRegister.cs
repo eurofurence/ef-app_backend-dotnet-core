b8adc76ee0c4deeee96ceed70326dadad35eacc2
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2025-09-01 23:14:22 +0200

    fix(l&f): temporary workaround for app bug

---
+++ src/Eurofurence.App.Domain.Model/LostAndFound/LostAndFoundResponseRegister.cs
@@ -0,0 +1,20 @@
+﻿using Eurofurence.App.Domain.Model.LostAndFound;
+using Mapster;
+
+namespace Eurofurence.App.Server.Web.Mapper
+{
+    /// <summary>
+    /// Mapping register for mapping IDs of lost and found records to the response type
+    /// </summary>
+    public class LostAndFoundResponseRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config
+                .NewConfig<LostAndFoundRecord, LostAndFoundResponse>()
+                //FIXME: workaround for EF29 version of app using LastChangeDateTimeUtc instead of Found/LostDateTimeUtc
+                .Map(dest => dest.LastChangeDateTimeUtc, src => src.Status == LostAndFoundRecord.LostAndFoundStatusEnum.Found ? src.FoundDateTimeUtc : src.LostDateTimeUtc)
+                .PreserveReference(true);
+        }
+    }
+}
