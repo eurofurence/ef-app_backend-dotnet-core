37bed98a8eeead8dede3dc9c76f45f856cb031e5
Maakinoh <29598855+maakinoh@users.noreply.github.com>
2026-04-10 19:13:31 +0200

    feat(#291): add endpoint for creating a matrix code from the user reg id

--- src/Eurofurence.App.Server.Services/Abstractions/Identity/IIdentityService.cs
+++ src/Eurofurence.App.Server.Services/Abstractions/Identity/IIdentityService.cs
@@ -16,5 +16,12 @@ namespace Eurofurence.App.Server.Services.Abstractions.Identity
         public Task<IEnumerable<string>> GetGroupMembers(string groupId);
         public Task<List<string>> GetCachedGroupMembers(string groupId,
         CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Generates a matrix code for the user based on their reg ID as a svg file.
+        /// </summary>
+        /// <param name="identity">Users identity</param>
+        /// <returns>The svg image data</returns>
+        public string GenerateUserMatrixCode(ClaimsIdentity identity);
     }
 }
