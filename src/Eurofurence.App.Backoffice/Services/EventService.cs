fc54310d6491657fbea6158d1c5321bcd69d672d
Meta <76539710+Metawolve@users.noreply.github.com>
2026-07-07 18:58:53 +0200

    feat: Added event page with day filter and favorite count

---
+++ src/Eurofurence.App.Backoffice/Services/EventService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Eurofurence.App.Domain.Model.Events;
+using static System.Net.WebRequestMethods;
+
+namespace Eurofurence.App.Backoffice.Services
+{
+    public class EventService(HttpClient http) : IEventService
+    {
+        public async Task<EventResponse[]> GetEventsAsync()
+        {
+            return (await http.GetFromJsonAsync<EventResponse[]>("events"))?.ToArray() ?? [];
+        }
+
+        public async Task<EventConferenceDayResponse[]> GetEventConferenceDaysAsync()
+        {
+            return (await http.GetFromJsonAsync<EventConferenceDayResponse[]>("eventConferenceDays"))?.ToArray() ?? [];
+        }
+    }
+}
