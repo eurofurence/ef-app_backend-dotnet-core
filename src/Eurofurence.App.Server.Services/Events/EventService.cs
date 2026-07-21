8e413aa763b6562aa2d9b8014e15c91f172c0b57
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2026-07-21 13:13:27 +0200

    fix(event): avoid trying to add same favorite again

--- src/Eurofurence.App.Server.Services/Events/EventService.cs
+++ src/Eurofurence.App.Server.Services/Events/EventService.cs
@@ -83,12 +83,11 @@ namespace Eurofurence.App.Server.Services.Events
                 .Include(userRecord => userRecord.FavoriteEvents)
                 .FirstOrDefault(x => x.IdentityId == user.GetSubject());
 
-            if (userRecord != null && !userRecord.FavoriteEvents.Contains(eventRecord))
+            if (userRecord != null && userRecord.FavoriteEvents.FirstOrDefault(favorite => favorite.Id == eventRecord.Id) is null)
             {
                 userRecord.FavoriteEvents.Add(eventRecord);
+                await _appDbContext.SaveChangesAsync();
             }
-
-            await _appDbContext.SaveChangesAsync();
         }
 
         /// <summary>
