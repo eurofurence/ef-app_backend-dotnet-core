870d43dfe35204659d19730078fdbf004afed8bc
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2026-07-31 16:28:58 +0200

    fix(web): use convention tz in event preview (#482)
    * fix(web): avoid hard-coded URL in EventsPreview
    
    * fix(docker): switch to -extra image to have access to tzdata
    
    * fix(web): use convention tz in event preview
    
    * chore: simplify GlobalOptions.ConventionTimeZoneInfo

--- src/Eurofurence.App.Server.Services/Abstractions/GlobalOptions.cs
+++ src/Eurofurence.App.Server.Services/Abstractions/GlobalOptions.cs
@@ -4,6 +4,8 @@ namespace Eurofurence.App.Server.Services.Abstractions
 {
     public class GlobalOptions
     {
+        private TimeZoneInfo _conventionTimeZoneInfo;
+
         /// <summary>
         /// Unique identifier for the convention and iteration (e.g. "EF30").
         /// </summary>
@@ -58,6 +60,15 @@ namespace Eurofurence.App.Server.Services.Abstractions
         /// URL or email address via which the convention can be contacted.
         /// </summary>
         public string ConventionContact { get; init; }
+        /// <summary>
+        /// Timezone identifier used when rendering UTC dates in web previews.
+        /// </summary>
+        public string ConventionTimeZone { get; init; }
+        /// <summary>
+        /// TimeZoneInfo object based on time zone name in <c>ConventionTimeZone</c>.
+        /// </summary>
+        public TimeZoneInfo ConventionTimeZoneInfo =>
+            _conventionTimeZoneInfo ??= TimeZoneInfo.FindSystemTimeZoneById(ConventionTimeZone);
         /// <summary>
         /// TODO: Seems to be unused? Check and potentially drop during next cleanup.
         /// </summary>
