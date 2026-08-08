14c7e6244ebb179024bf7e803d132c228de67cb4
Maakinoh <29598855+maakinoh@users.noreply.github.com>
2026-08-08 23:06:12 +0200

    fix(announcements): extend access check to include AnnouncementManager role

--- src/Eurofurence.App.Server.Services/Announcements/AnnouncementService.cs
+++ src/Eurofurence.App.Server.Services/Announcements/AnnouncementService.cs
@@ -83,7 +83,7 @@ namespace Eurofurence.App.Server.Services.Announcements
                     _appDbContext.Announcements
                         .Where(entity =>
                             entity.IsDeleted == 0
-                            && (entity.Groups == null || !entity.Groups.Any() || entity.Groups.Any(group => userGroups.Contains(group))))
+                            && (entity.Groups == null || !entity.Groups.Any() || entity.Groups.Any(group => userGroups.Contains(group)) || _httpContext.User.IsInRole(IdentityRoles.AnnouncementManager)))
                         .Select(x => x.Transform()).ToArrayAsync(cancellationToken);
             }
             else
@@ -94,7 +94,7 @@ namespace Eurofurence.App.Server.Services.Announcements
                     .IgnoreQueryFilters()
                     .Where(entity =>
                         entity.LastChangeDateTimeUtc > minLastDateTimeChangedUtc
-                        && (entity.Groups == null || !entity.Groups.Any() || entity.Groups.Any(group => userGroups.Contains(group))));
+                        && (entity.Groups == null || !entity.Groups.Any() || entity.Groups.Any(group => userGroups.Contains(group)) || _httpContext.User.IsInRole(IdentityRoles.AnnouncementManager)));
 
                 response.ChangedEntities = await entities
                     .Where(a => a.IsDeleted == 0)
