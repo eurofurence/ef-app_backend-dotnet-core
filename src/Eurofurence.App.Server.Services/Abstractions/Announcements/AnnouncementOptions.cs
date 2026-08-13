ab704f51ef0de06f25e4a67fedfc355665e8e3de
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2026-08-13 23:48:41 +0200

    feat(announcements): set validity via configuration

--- src/Eurofurence.App.Server.Services/Abstractions/Announcements/AnnouncementOptions.cs
+++ src/Eurofurence.App.Server.Services/Abstractions/Announcements/AnnouncementOptions.cs
@@ -1,7 +1,16 @@
-﻿namespace Eurofurence.App.Server.Services.Abstractions.Announcements
+﻿#nullable enable
+namespace Eurofurence.App.Server.Services.Abstractions.Announcements
 {
     public class AnnouncementOptions
     {
-        public string Url { get; init; }
+        /// <summary>
+        /// URL of the endpoint providing the latest announcements.
+        /// </summary>
+        public string? Url { get; init; }
+        /// <summary>
+        /// Hours from creation that an announcement should be considered valid.
+        /// Defaults to half a year.
+        /// </summary>
+        public int AnnouncementValidityHours { get; init; } = 24 * 183;
     }
 }
